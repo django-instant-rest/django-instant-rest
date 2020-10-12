71e075be3ca064b603666847e991bd1c5c678019
craig fay <craigfay1@gmail.com>
2020-10-12 12:25:38 -0400

    Remove package artifacts

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='django_instant_rest',
+      version='0.1',
+      description='',
+      url='http://github.com/zero-gravity-computer/django_instant_rest',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['django_instant_rest'],
+      zip_safe=False)
