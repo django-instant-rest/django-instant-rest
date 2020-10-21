2821e2db83acf812b53f5a462a800bf091534247
unknown <jbj1996@gmail.com>
2020-10-21 10:11:33 -0400

    totally added a thing to the thing about the thing... python >=3.7 requirement

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='django_instant_rest',
       version='0.1',
       description='',
+      python_requires='>=3.7'
       url='http://github.com/zero-gravity-computer/django_instant_rest',
       author='',
       author_email='',
