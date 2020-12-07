d0b1835702c87f8067711bec8c60bef6b05f4a33
John Johnson <67648868+J4mesB4xter@users.noreply.github.com>
2020-12-07 11:37:29 -0500

    Update setup.py
    found a comma

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='django_instant_rest',
       version='0.1',
       description='',
-      python_requires='>=3.7'
+      python_requires='>=3.7',
       url='https://github.com/django-instant-rest/django-instant-rest.git',
       author='',
       author_email='',
