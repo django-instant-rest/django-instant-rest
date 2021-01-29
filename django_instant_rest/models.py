a3273c8bc952db28501c046d5e41e3de73a75b88
unknown <jbj1996@gmail.com>
2021-01-28 21:52:09 -0500

    added moved serializer into TimeStampedModel renamed it RestResource

--- django_instant_rest/models.py
+++ django_instant_rest/models.py
@@ -1,12 +1,26 @@
 from django.db import models
+import datetime
 
-class TimeStampedModel(models.Model):
+class RestResource(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     class Meta:
         abstract = True
 
-#to dict serialization could be included
-#in this model to make objects json by default
-#name change would be rest-resource
+    def to_dict(self):
+        result = {}
+        for field in self._meta.fields:
+            value = getattr(self, field.name)
+
+            #handles datetime fields
+            if type(value) == datetime.datetime:
+                value = value.isoformat()
+
+            #handles relational fields 
+            if hasattr(value, "id"):
+                value = value.id
+            
+            result[field.name] = value
+        return result
 
