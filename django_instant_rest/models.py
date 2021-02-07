06d6ff932c25acf9acea69a2ce79aa0f923a8736
craig fay <craigfay1@gmail.com>
2021-02-07 14:01:17 -0600

    Introduce RestClient model, improve error handling

--- django_instant_rest/models.py
+++ django_instant_rest/models.py
@@ -1,3 +1,4 @@
+from argon2 import PasswordHasher
 from django.db import models
 import datetime
 
@@ -9,6 +10,7 @@ class RestResource(models.Model):
         abstract = True
 
     def to_dict(self):
+        '''Convert model instances to dictionaries'''
         result = {}
         for field in self._meta.fields:
             value = getattr(self, field.name)
@@ -24,3 +26,22 @@ class RestResource(models.Model):
             result[field.name] = value
         return result
 
+
+
+class RestClient(models.Model):
+    username = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=512)
+
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        '''Saving the model instance, but first hashing the
+        plaintext password stored in it's `password` field'''
+        self.password = PasswordHasher().hash(self.password)
+        super(RestClient, self).save(*args, **kwargs)
+
+    def verify_password(self, password):
+        '''Determine whether a given hashed password belongs
+        to this model instance'''
+        return ph.verify(self.password, password)
--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup
 
 setup(name='django_instant_rest',
-      version='0.1',
-      description='',
-      python_requires='>=3.7',
-      url='https://github.com/django-instant-rest/django-instant-rest.git',
-      author='',
-      author_email='',
-      license='MIT',
-      packages=['django_instant_rest'],
-      zip_safe=False)
+    version='0.1',
+    description='',
+    python_requires='>=3',
+    url='https://github.com/django-instant-rest/django-instant-rest.git',
+    author='',
+    author_email='',
+    license='MIT',
+    zip_safe=False,
+    packages=['django_instant_rest'],
+    install_requires=[
+        'setuptools',
+        'argon2-cffi>=20.1',
+        'pyjwt>=2',
+    ],
+)
