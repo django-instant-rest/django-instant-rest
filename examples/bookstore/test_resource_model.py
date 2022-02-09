3d962b2afcc15d2dcd67424c5577140c9150ccbd
craigfay <craigfay1@gmail.com>
2022-02-09 01:30:10 -0600

    Test pagination, fields

--- examples/bookstore/test_resource_model.py
+++ examples/bookstore/test_resource_model.py
@@ -18,20 +18,31 @@ class ResourceModelTests(TestCase):
         self.author = Author.objects.create(first_name="Agatha", last_name="Christie")
         self.author = Author.objects.create(first_name="Akira", last_name="Toriyama")
 
-    def test_can_get_resource(self):
+    def test_get_requests_return_200(self):
         request = self.factory.get('/authors')
         response = self.author_view.callback(request)
         self.assertEqual(response.status_code, 200)
 
+
+    def test_get_requests_return_model_instances(self):
+        request = self.factory.get('/authors')
+        response = self.author_view.callback(request)
         body = json.loads(response.content)
         self.assertEqual(len(body['data']), 3)
 
+    def test_get_requests_return_model_instance_fields(self):
+        request = self.factory.get('/authors')
+        response = self.author_view.callback(request)
+        body = json.loads(response.content)
+
         author = body['data'][0]
+        self.assertIsNotNone(author['id'])
         self.assertEqual(author['first_name'], 'Stephen')
         self.assertEqual(author['last_name'], 'King')
+        self.assertIsInstance(author['created_at'], str)
+        self.assertIsInstance(author['updated_at'], str)
 
-
-    def test_pagination_cursors(self):
+    def test_get_requests_return_pagination_cursors(self):
         request = self.factory.get('/authors')
         response = self.author_view.callback(request)
 
