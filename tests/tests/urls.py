3b2dac3c4e249e63fe90e8fc3dcc4aa66da561bb
craigfay <craigfay1@gmail.com>
2022-10-07 01:50:19 -0500

    Add backwards rels to gql types

--- tests/tests/urls.py
+++ tests/tests/urls.py
@@ -11,9 +11,9 @@ from textwrap import dedent
 def gql_primitive(field):
     field_type = type(field)
     if field_type == models.BigAutoField:
-        return 'Int'
+        return 'ID'
     elif field_type == models.ForeignKey:
-        return 'Int'
+        return field.related_model.__name__
     elif field_type == models.DateTimeField:
         return 'String'
     elif field_type == models.CharField:
@@ -21,23 +21,56 @@ def gql_primitive(field):
 
     return 'String'
 
-
-def model_as_gql_type_def(model):
-    gql_fields = [f"{f.name}: {gql_primitive(f)}" for f in model._meta.fields]
-    newline = "\n    "
-
-    return (f"type {model.__name__} {{\n" 
-        f"    {newline.join(gql_fields)}\n"
-        "}\n"
-    )
+def backwards_rel_type(set):
+    typename = set.rel.related_model.__name__
+    return typename if not set.rel.multiple else f"[{typename}]"
 
 
+class GraphQLField():
+    def __init__(self, field):
+        self.name = field.name
+        self.typename = gql_primitive(field)
+
+class GraphQLBackwardsRel():
+    def __init__(self, set):
+        self.name = set.rel.name
+        self.typename = backwards_rel_type(set)
+
+
+class GraphQLModel():
+    def __init__(self, model):
+        self.name = model.__name__
+        self.fields = [GraphQLField(f) for f in model._meta.fields]
+
+        backwards_rel_attrs = list(filter(lambda p: p.endswith('set'), dir(model)))
+        backwards_rel_sets = [getattr(model, attr) for attr in backwards_rel_attrs]
+        self.fields += [GraphQLBackwardsRel(set) for set in backwards_rel_sets]
+
+
+    def stringify(self):
+        gql_fields = [f"{f.name}: {f.typename}" for f in self.fields]
+        newline = "\n    "
+
+        return (f"type {self.name} {{\n" 
+            f"    {newline.join(gql_fields)}\n"
+            "}\n"
+        )
 
 
 included_models = [Book, Author]
-gql_model_types = "\n".join([model_as_gql_type_def(m) for m in included_models])
-print(gql_model_types)
+gql_models = map(lambda m: GraphQLModel(m), included_models)
+print("\n".join([ m.stringify() for m in gql_models ]))
 
+# print(dir(Author.book_set.rel))
+# print(Author.book_set.rel.name)
+# print(Author.book_set.rel.related_model.__name__)
+# print(Author.book_set.rel.many_to_many)
+# print(Author.book_set.rel.many_to_one)
+# print(Author.book_set.rel.one_to_one)
+# print(Author.book_set.rel.one_to_many)
+# print(Author.book_set.rel.multiple)
+
+# print(dir(Author))
 
 type_defs = gql("""
     type Query {
